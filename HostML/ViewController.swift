2280cdcf623624b9b93f2ff00a2620283043ea53
NDAMODA <harman.damodar@daimler.com>
2020-10-03 19:21:20 +0530

    Initial Commit

---
+++ HostML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HostML
+//
+//  Created by Damodar, Namala (623-Extern) on 03/10/20.
+//  Copyright © 2020 Damodar, Namala (623-Extern). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
