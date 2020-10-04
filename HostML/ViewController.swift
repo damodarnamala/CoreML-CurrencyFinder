e5e591f045b297d2367b69851c30a6ffbde857b9
NDAMODA <harman.damodar@daimler.com>
2020-10-04 22:23:44 +0530

    Finding Indian currency with CooreML

--- HostML/ViewController.swift
+++ HostML/ViewController.swift
@@ -7,14 +7,88 @@
 //
 
 import UIKit
+import Vision
+import CoreML
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    @IBOutlet weak var labelReult: UILabel?
+    
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
+        
+        setupCaptureSession()
     }
-
-
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+    
+    @objc func rotated() {
+        DispatchQueue.main.async(execute: { [weak self] in
+            guard let buounds = self?.view.bounds else {return}
+            self?.previewLayer?.frame = buounds
+        })
+        
+    }
+    
+    
+    func setupCaptureSession() {
+        
+        // creates a new capture session
+        let captureSession = AVCaptureSession()
+        
+        // search for available capture devices
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        
+        // get capture device, add device input to capture session
+        do {
+            if let captureDevice = availableDevices.first {
+                captureSession.addInput(try AVCaptureDeviceInput(device: captureDevice))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // setup output, add output to capture session
+        let captureOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(captureOutput)
+        
+        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.previewLayer?.frame = view.bounds
+        self.previewLayer?.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(self.previewLayer!)
+        captureSession.startRunning()
+    }
+    
+    
+    // called everytime a frame is captured
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let model = try? VNCoreMLModel(for: Notes().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let Observation = results.first else { return }
+            
+            DispatchQueue.main.async(execute: {
+                if Observation.confidence >  0.95 {
+                    self.labelReult?.layer.cornerRadius = 4
+                    self.labelReult?.text = " \(Observation.identifier) Rupees  "
+                    self.labelReult?.layer.zPosition = 1
+                }
+            })
+        }
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        // executes request
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
 }
+
 
